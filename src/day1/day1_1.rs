4208da11d3cbb26c316ec82d3c1ab6abde796ce6
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2023-12-07 15:51:30 +0100

    Day 1 part 1

--- src/day1/day1_1.rs
+++ src/day1/day1_1.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 pub fn day1_1() {
-    println!("hello")
+    let input: String = fs::read_to_string("src/day1/input.txt")
+        .expect("Should have been able to read the file");
+
+    let lines: Vec<&str> = input.split("\r\n").collect();
+
+    let digit_lines: Vec<String> = lines
+        .iter()
+        .map(|x| x.chars().filter(|y| y.is_numeric()).collect())
+        .collect();
+
+    let result: u32 = digit_lines
+        .iter()
+        .map(|x: &String| {
+            if let (Some(first_digit), Some(last_digit)) = (x.chars().nth(0), x.chars().last()) {
+                if let (Some(first), Some(last)) = (first_digit.to_digit(10), last_digit.to_digit(10)) {
+                    return first * 10 + last;
+                }
+            }
+            return 0;
+        })
+        .sum();
+
+    println!("{result}");
 }
