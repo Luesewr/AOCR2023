9bfb2ad04d2278ce73fed57fffcab412d3afb212
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2023-12-07 16:10:51 +0100

    Day 1 part 2

---
+++ src/day1/day1_2.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+use std::fs;
+
+pub fn day1_2() {
+    let input: String = fs::read_to_string("src/day1/input.txt")
+        .expect("Should have been able to read the file");
+
+    let map: HashMap<&str, &str> = HashMap::from([
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "th3ee"),
+        ("four", "fo4r"),
+        ("five", "fi5e"),
+        ("six", "s6x"),
+        ("seven", "se7en"),
+        ("eight", "ei8ht"),
+        ("nine", "ni9e"),
+    ]);
+
+    let mut lines: Vec<String> = input.split("\r\n").map(String::from).collect();
+
+    for line in lines.iter_mut() {
+        for (digit, replacement) in &map {
+            while line.contains(digit) {
+                let index: usize = line.find(digit).unwrap();
+                line.replace_range(index..(index + digit.len()), replacement);
+            }
+        }
+    }
+
+    let digit_lines: Vec<String> = lines
+        .iter()
+        .map(|x| x.chars().filter(|y| y.is_numeric()).collect())
+        .collect();
+
+    let result: u32 = digit_lines
+        .iter()
+        .map(|x: &String| {
+            if let (Some(first_digit), Some(last_digit)) = (x.chars().nth(0), x.chars().last()) {
+                if let (Some(first), Some(last)) = (first_digit.to_digit(10), last_digit.to_digit(10)) {
+                    return first * 10 + last;
+                }
+            }
+            return 0;
+        })
+        .sum();
+
+    println!("{result}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use crate::day1::day1_1;
+use crate::day1::day1_2;
 
 mod day1;
 
 fn main() {
-    day1_1()
+    // day1_1();
+    day1_2();
 }
